447ba15a4ddef33e0c9d75f34ea80e0647dd7eb9
Farhad <farhad.g@soft-artel.com>
2020-08-25 13:31:29 +0300

    Initial Commit

---
+++ PhoneExtractor2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhoneExtractor2
+//
+//  Created by Farhad Gatiyatov on 25.08.2020.
+//  Copyright © 2020 Farhad Gatiyatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
